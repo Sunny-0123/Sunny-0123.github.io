ffd00fe71938299cc378a087513cbeaf76f7fe88
周昊 <171540199@qq.com>
2023-06-27 13:15:04 +0800

    1

---
+++ dist/assets/js/6.329fef77.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{278:function(n,t,e){"use strict";e.r(t);var o={name:"my-button",methods:{handleClick(){console.log("我是一个按钮111")}}},l=e(14),c=Object(l.a)(o,(function(){return(0,this._self._c)("button",{on:{click:this.handleClick}},[this._v("我是一个按钮")])}),[],!1,null,"5f8e86e0",null);t.default=c.exports}}]);
